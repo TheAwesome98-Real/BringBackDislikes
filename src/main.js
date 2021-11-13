c3d68b37234073aea932c39143a6c2d39b22ff3a
TheAwesome98 <TheAwesome98@programmer.net>
2021-11-13 16:35:45 +0000

    i don't have a brain

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 var dislikeCount = document.querySelector(".top-level-buttons").getElementsByTagName("ytd-toggle-button-renderer")[1].getElementsByTagName("a")[0].getElementsByTagName("yt-formatted-string")[0];
 
-if (!dislikeCount.innerText.toLowerCase() == "dislike") {
+if (dislikeCount.innerText.toLowerCase() != "dislike") {
   // TODO: send to me!!!!!
 } else {
   alert("test");
